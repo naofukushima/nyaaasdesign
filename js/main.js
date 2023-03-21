81e0417e58cfe3adbd06aae0ac926f44409d6694
naofukushima <122948784+naofukushima@users.noreply.github.com>
2023-03-21 15:42:41 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,1 @@
+$(function() {	/*バーガーメニューのクリックイベント*/	$('.toggle_btn').on('click',function() {		if ($('#header').hasClass('open')) {			$('#header').removeClass('open');		}		else {			$('#header').addClass('open');		}	});});$('#mask').on('click',function() {	$('#header').removeClass('open');});
